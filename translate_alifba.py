211c02d03e322e26047b13b8f183112165baaa67
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2017-11-30 17:31:01 +0300

    оформление в виде функций

--- translate_alifba.py
+++ translate_alifba.py
@@ -2,46 +2,63 @@ import configparser
 
 namefile_cfg = "cfg-alifba.cfg"
 
-with open(namefile_cfg,"r") as f:
-    lines = f.readlines()
 
-# print(lines)
 
-common_turic_alifbasi_to_cyrilic_tatar = dict()
-cyrilic_tatar_to_common_turic_alifbasi = dict()
-for line in lines:
-    line = line.replace("\n","")
-    l = line.split("=")
-    turk = l[0]
-    cyrilic = l[1]
-    if turk in common_turic_alifbasi_to_cyrilic_tatar:
-        common_turic_alifbasi_to_cyrilic_tatar[turk]=common_turic_alifbasi_to_cyrilic_tatar[turk]+"*"+cyrilic
-    else:
-        common_turic_alifbasi_to_cyrilic_tatar[turk]=cyrilic
-    if cyrilic in cyrilic_tatar_to_common_turic_alifbasi:
-        cyrilic_tatar_to_common_turic_alifbasi[cyrilic]=cyrilic_tatar_to_common_turic_alifbasi[cyrilic]+"*"+turk
-    else:
-        cyrilic_tatar_to_common_turic_alifbasi[cyrilic]=turk
+def get_alifbasi(namefile_cfg):
+    """ наполнение словарей перевода одних букв в другие """
+    with open(namefile_cfg,"r") as f:
+        lines = f.readlines()
+
     
+    common_turic_alifbasi_to_cyrilic_tatar = dict() # из общетюркского алфавита в кирилический татарский
+    cyrilic_tatar_to_common_turic_alifbasi = dict() # из  кирилического татарского в общетюркский алфавит
+    for line in lines:
+        line = line.replace("\n","")
+        l = line.split("=")
+        turk = l[0]
+        cyrilic = l[1]
+        if turk in common_turic_alifbasi_to_cyrilic_tatar:
+            common_turic_alifbasi_to_cyrilic_tatar[turk]=common_turic_alifbasi_to_cyrilic_tatar[turk]+"*"+cyrilic
+        else:
+            common_turic_alifbasi_to_cyrilic_tatar[turk]=cyrilic
+        if cyrilic in cyrilic_tatar_to_common_turic_alifbasi:
+            cyrilic_tatar_to_common_turic_alifbasi[cyrilic]=cyrilic_tatar_to_common_turic_alifbasi[cyrilic]+"*"+turk
+        else:
+            cyrilic_tatar_to_common_turic_alifbasi[cyrilic]=turk
+    return cyrilic_tatar_to_common_turic_alifbasi, cyrilic_tatar_to_common_turic_alifbasi
+    # END наполнение словарей перевода одних букв в другие 
 
-# print(common_turic_alifbasi_to_cyrilic_tatar)
-# print()
-# print(cyrilic_tatar_to_common_turic_alifbasi)
-    
+# перевод из кирилицы в общетюркский алфавит
+def translate_cyrilic_to_cta(original_text,cyrilic_tatar_to_common_turic_alifbasi):
+    """ перевод из кирилицы в общетюркский алфавит
+            original_text - текст который нужно перевести
+            cyrilic_tatar_to_common_turic_alifbasi - 
+     """
+
+    translate_text = "" # перевод текста
+    # TODO: сначала надо отсканировать все сочетания символов которые по два или более символа.
+    # TODO: надо научиться помечать/обрабатывать смволы у которых может быть два варианта.
+    for i in original_text:  
+        try:       
+            if i.isupper():
+                sim = i.lower()
+                translate_text+=(cyrilic_tatar_to_common_turic_alifbasi[sim]).upper()
+            else:
+                translate_text+=cyrilic_tatar_to_common_turic_alifbasi[i]
+        except KeyError:
+            translate_text+=i
+    return translate_text
+# END перевод из кирилицы в общетюркский алфавит
+
+
+#     
 # original_text = "әйбЕт"
 #original_text = "Казань"
-original_text = "Объявлениясы һава.!"
+original_text = "Татар телен укытудан баш тартмаган Шмаков мәхкәмәдә прокуратура ялганын"
 print(original_text)
-translate_text = "" # перевод тес\кста
-# TODO: сначала надо отсканировать все сочетания символов которые по два или более символа.
-# TODO: надо научиться помечать/обрабатывать смволы у которых моңет бытҗ два варианта.
-for i in original_text:  
-    try:       
-        if i.isupper():
-            sim = i.lower()
-            translate_text+=(cyrilic_tatar_to_common_turic_alifbasi[sim]).upper()
-        else:
-            translate_text+=cyrilic_tatar_to_common_turic_alifbasi[i]
-    except KeyError:
-        translate_text+=i
-print(translate_text)
+cyr2cta, cta2cyr = get_alifbasi(namefile_cfg)
+
+text = translate_cyrilic_to_cta(original_text,cyr2cta)
+print(text)
+
+
